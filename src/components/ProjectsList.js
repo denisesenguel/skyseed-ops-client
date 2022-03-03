58e70b3d2ef7f9462e5db964a9e098b19c324fb0
denisesenguel <denisesenguel@gmail.com>
2022-03-03 18:25:24 +0100

    Fix click color with forbidden workaround :) Resolves #7

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -21,25 +21,27 @@ export default function ProjectsList() {
   }, []);
 
   return (
-    <div className="w-100">
-      <Container className="w-100">
-        <Row className="text-white bg-primary-cstm rounded p-3">
-          <Col xs={3}>Title</Col>
-          <Col xs={2}>When?</Col>
-          <Col xs={3}>Where?</Col>
-          <Col xs={2}>status</Col>
-          <Col xs={2}>Last Update</Col>
-        </Row>
+    <div>
+      <Container className="w-100 m-5">
+      <h1 className="mb-5">All Projects</h1>
+      <ListGroup>
+        <ListGroup.Item className="text-white bg-primary-cstm rounded p-3">
+          <Row>
+            <Col xs={3}>Title</Col>
+            <Col xs={2}>When?</Col>
+            <Col xs={3}>Where?</Col>
+            <Col xs={2}>status</Col>
+            <Col xs={2}>Last Update</Col>
+          </Row>
+        </ListGroup.Item>
         {
           (projects.length === 0) ?
             <p>No projects found.</p> :
-            <ListGroup>
-            {
-              projects?.map((project) => (
+              projects.map((project) => (
                 <ListGroup.Item action 
                   key={ project._id }
                   href={`/home/projects/${project._id}`} 
-                  className="text-primary-cstm shadow rounded p-4 my-2 active-green hover-beige">
+                  className="active-green text-primary-cstm shadow rounded p-4 my-2">
                   <Row>
                     <Col xs={3}>{ project.title }</Col>
                     <Col xs={2}>{ [project.season, project.year].join(" ") } </Col>
@@ -56,9 +58,8 @@ export default function ProjectsList() {
                   </Row>
                 </ListGroup.Item>
               ))
-            }
-            </ListGroup>
         }
+      </ListGroup>
       </Container>
     </div>
   )
