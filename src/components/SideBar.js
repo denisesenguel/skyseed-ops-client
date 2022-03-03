ec1cb7cd37d9667751e904d0133ed12834d7adc7
denisesenguel <denisesenguel@gmail.com>
2022-03-03 22:38:11 +0100

    Got rid of annoying warnings

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -19,24 +19,25 @@ function SideBar() {
 
         <CDBSidebarContent className="sidebar-content">
           <CDBSidebarMenu>
-            <NavLink exact to="/home" activeClassName="activeClicked">
+            {/* might want to assign activeClassName="activeClicked" to these NavLinks */}
+            <NavLink exact="true" to="/home" >
               <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
             </NavLink>
             <h6 className="mx-4 mt-3">Projects</h6>
-            <NavLink exact to="/home/projects" activeClassName="activeClicked">
+            <NavLink exact="true" to="/home/projects">
                 <CDBSidebarMenuItem icon="tree">View All</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/home/projects/my-projects" activeClassName="activeClicked">
+            <NavLink exact="true" to="/home/projects/my-projects">
                 <CDBSidebarMenuItem icon="tree">My Projects</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/home/projects/create" activeClassName="activeClicked">
+            <NavLink exact="true" to="/home/projects/create">
                 <CDBSidebarMenuItem icon="plus">Add new</CDBSidebarMenuItem>
             </NavLink>
             <h6 className="mx-4 mt-3">Customers</h6>
-            <NavLink exact to="/home/customers" activeClassName="activeClicked">
+            <NavLink exact="true" to="/home/customers">
               <CDBSidebarMenuItem icon="list">View All</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/home/customers/create" activeClassName="activeClicked">
+            <NavLink exact="true" to="/home/customers/create">
               <CDBSidebarMenuItem icon="plus">Add new</CDBSidebarMenuItem>
             </NavLink>
           </CDBSidebarMenu>
