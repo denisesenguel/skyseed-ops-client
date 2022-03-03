6ba0c47e2deacf9468c9bcdb585e3732de73dae1
denisesenguel <denisesenguel@gmail.com>
2022-03-03 20:47:36 +0100

    first version Project Details

---
+++ src/components/ProjectSummary.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Button, Form, Container, Row, Col, Card } from 'react-bootstrap';
+import userImg from '../images/domi_small.png';
+
+export default function ProjectSummary(props) {
+    
+    const { project } = props;
+    
+    return (
+        <div className="mt-3">
+            <Form>
+                <Form.Group controlId="description">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control 
+                        readOnly={ true }
+                        className="bg-white"
+                        as="textarea" rows={3} placeholder="Give a short description"
+                        value={ project.description } 
+                    />
+                </Form.Group>
+                <Form.Group controlId="sizeInHa">
+                    <Form.Label>Size (in ha)</Form.Label>
+                    <Form.Control 
+                        readOnly={ true }
+                        type="number" 
+                        className="bg-white"
+                        value={ project.sizeInHa } 
+                    />
+                </Form.Group>
+                <h6 className="text-secondary-cstm my-3">More Infos following later...</h6>
+            </Form>
+            <h6>Project Managers</h6>
+            <Container fluid>
+                <Row>
+                {
+                    (!project || !project?.managers || project?.managers?.length === 0) ?
+                        <p> None assigned yet.</p> :
+                        project.managers.map((user) => (
+                            <Col xs={2} className="mx-0 px-0 text-primary-cstm d-flex align-items-center" key={ user._id }>
+                                <Card>
+                                    <Card.Img variant="top" src={ userImg }></Card.Img>
+                                    <h6 className="text-center">Denise</h6>
+                                    <Button size="sm" variant="custom" className="bg-secondary-cstm">Send Email</Button>
+                                </Card>
+                            </Col>
+                        ))
+                                
+                }
+                </Row>
+            </Container>
+
+            <div className="d-flex justify-content-end">
+                {/* <Button variant="custom" className="bg-secondary-cstm mx-2" >Edit Details</Button> */}
+                <Button variant="custom" className="bg-error-cstm text-neutral-grey">Delete this project</Button>
+            </div>
+        </div>
+
+    )
+}
--- src/pages/ProjectDetailsPage.js
+++ src/pages/ProjectDetailsPage.js
@@ -1,16 +1,62 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { Nav, Button } from 'react-bootstrap';
+import axios from 'axios';
+import ProjectSummary from '../components/ProjectSummary';
 
 export default function ProjectDetailsPage() {
+
+  const { projectId } = useParams();
+  const [project, setProject] = useState({});
+  const [selectedTab, setSelectedTab] = useState("summary");
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/projects/${projectId}`)
+      .then((response) => {
+        console.log("server response: ", response.data)
+        setProject(response.data)
+      }
+      )
+      .catch((error) => console.log("Error getting project: ", error))
+  }, [])
+
+
   return (
-    <div>
-        ProjectDetailsPage
-        <Button>Edit</Button>
-        <Button>Delete</Button>
-        <Routes>
-            <Route path="/projects/:projectId/edit" element={ <div>Edit Project</div>} />
-        </Routes>
+    <div className="m-5 w-100">
+        <h4>{ project.title } - { project.season } { project.year }</h4>
+        <p>{ project.location }</p>
+        <div className="w-100 d-flex justify-content-between align-items-center">
+        <Link 
+          className="text-decoration-none text-primary-cstm" 
+          to={ `/home/customers/${project.customer?._id}` }>
+          <h6>Client: { [project.customer?.firstName, project.customer?.lastName].join(" ") }</h6>
+        </Link>
+        <Button size="sm" variant="custom" className="bg-secondary-cstm">Email Client</Button>
+        </div>
+
+        <Nav 
+          fill variant="tabs" 
+          className="mt-4"
+          defaultActiveKey="summary" 
+          onSelect={ (key) => setSelectedTab(key) }>
+          <Nav.Item>
+            <Nav.Link className="text-primary-cstm" eventKey="summary">Summary</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="text-primary-cstm" eventKey="details">Sowing Details</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="text-primary-cstm" eventKey="checklist">
+              Checklist
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+
+        { selectedTab === 'summary' && <ProjectSummary project={ project }/> }
+        { selectedTab === 'details' && <div> Render Sowing details here </div> }
+        { selectedTab === 'checklist' && <div> Render summary here </div> }
+
     </div>
   )
 }
