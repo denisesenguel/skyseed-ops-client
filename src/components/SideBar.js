118280e9faadbfb05794c333caf76c6903ada5d8
denisesenguel <denisesenguel@gmail.com>
2022-03-08 10:57:30 +0100

    basics done

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,81 +1,33 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import * as RiIcons from "react-icons/ri";
 import * as CgIcons from "react-icons/cg";
 import { IconContext } from "react-icons/lib";
+import { AuthContext } from "../context/auth.context";
 
-const SidebarNav = styled.nav`
-background: #15171c;
-width: ${({ sidebar }) => (sidebar ? "250px" : "60px")};
-height: 100vh;
-display: flex;
-justify-content: center;
-position: fixed;
-top: 60px;
-left: 0px;
-transition: 350ms;
-z-index: 10;
-`;
 // bg-custom-primary fix-sidebar-width (add position, transition, top and left to it!) fix-content-height d-flex justify-content-center position-fixed (?)
+// fix-sidebar
+// sidebar-width-hidden
 
-const SidebarLink = styled(Link)`
-display: flex;
-color: #e1e9fc;
-justify-content: space-between;
-align-items: center;
-padding: 20px;
-list-style: none;
-height: 60px;
-text-decoration: none;
-font-size: 18px;
+// navlink:
+// font-size: 18px;
+// &:hover {
+// 	background: #252831;
+// 	border-left: 4px solid green;
+// 	cursor: pointer;
+// };
 
-&:hover {
-	background: #252831;
-	border-left: 4px solid green;
-	cursor: pointer;
-}
-`;
-
-// className= "d-flex text-color-neutral-grey justify-content-space-between align-iemts-center p-5 list-style-none"
-// hover: later
-
-const span = styled.span`
-margin-left: 16px;
-`;
-// mx-4
-
-const DropdownLink = styled(Link)`
-background: #252831;
-height: 60px;
-padding-left: 3rem;
-display: flex;
-align-items: center;
-text-decoration: none;
-color: #f5f5f5;
-font-size: 18px;
-
-&:hover {
-	background: green;
-	cursor: pointer;
-}
-`;
-// fix-height(?) p-1 d-flex align-items-center text-decoration-none text-color-neutral-grey 
-
-
-const SidebarWrap = styled.div`
-width: 100%;
-`;
-// w-100
 
 function Sidebar() {
 
   const [sidebar, setSidebar] = useState(true);
   const [subNav, setSubNav] = useState([false, false]);
-  
+  const { user } = useContext(AuthContext);
+
   const toggleSidebar = () => setSidebar(!sidebar);
+  
   function toggleSubNav(index) {
     const newList = [...subNav];
     newList[index] = !newList[index];
@@ -85,14 +37,17 @@ function Sidebar() {
 return (
 	<>
 	<IconContext.Provider value={{ color: "#F4F6F7" }}>
-		<SidebarNav sidebar={ sidebar }>
-		<SidebarWrap>
+		<nav className={ `bg-primary-cstm fix-sidebar-position fix-content-height d-flex justify-content-center position-fixed 
+                      ${ (sidebar) ? "fix-sidebar-width" : "fix-sidebar-width-hidden" }` }>
+		<div className="w-100">
 
-			
-        {
-          sidebar ?
+      {
+        sidebar ?
           <div className="d-flex justify-content-between align-items-center p-4 text-neutral-grey border-bottom">
-            <h6 className="m-0">Hello Person!</h6> 
+            <div>
+              <h6 className="m-0">Hello { user.firstName }!</h6> 
+              { (user.role === 'admin') && <p className="m-0 font-xs text-muted">You're an admin.</p> }
+            </div>
             <Link to="#" className="d-flex align-items-center">
               <AiIcons.AiOutlineClose onClick={ toggleSidebar } size="20px"/>
             </Link> 
@@ -102,16 +57,14 @@ return (
               <FaIcons.FaBars onClick={ toggleSidebar } size="25px"/>
             </Link>
           </div>
-        }
+      }
 			
-
       {
         sidebar &&
         <div>
-
           <Link 
             to="/home" 
-            className= "d-flex text-neutral-grey justify-content-space-between align-iemts-center p-4 text-decoration-none"
+            className= "d-flex text-neutral-grey justify-content-space-between align-iemts-center mt-4 p-4 text-decoration-none"
           >
             <div className="d-flex align-items-center">
               <RiIcons.RiHome2Line />
@@ -122,7 +75,7 @@ return (
           <Link 
             to="#" 
             onClick={ () => toggleSubNav(0) }
-            className= "d-flex text-neutral-grey justify-content-space-between align-iemts-center px-4 text-decoration-none"
+            className= "d-flex text-neutral-grey justify-content-between align-iemts-center p-4 text-decoration-none"
           >
           <div className="d-flex align-items-center">
             <CgIcons.CgTrees />
@@ -135,23 +88,32 @@ return (
           {
             subNav[0] &&
               <div>
-                <DropdownLink to="/home/projects">
+                <Link 
+                  to="/home/projects"
+                  className = "px-4 mx-4 py-2 d-flex align-items-center text-decoration-none text-neutral-grey"
+                >
                   <span> Browse All </span>
-                </DropdownLink>
-                <DropdownLink to="/home/projects/my-projects">
+                </Link>
+                <Link 
+                  to="/home/projects/my-projects"
+                  className = "px-4 mx-4 py-2 d-flex align-items-center text-decoration-none text-neutral-grey"
+                >
                   <span> My Projects </span>
-                </DropdownLink>
-                <DropdownLink to="/home/projects/create">
+                </Link>
+                <Link 
+                  to="/home/projects/create"
+                  className = "px-4 mx-4 py-2 d-flex align-items-center text-decoration-none text-neutral-grey"
+                >
                   {/* icon here? */}
                   <span> Add New </span>
-                </DropdownLink>
+                </Link>
               </div>
           }
 
           <Link 
             to="#" 
             onClick={ () => toggleSubNav(1) }
-            className= "d-flex text-neutral-grey justify-content-space-between align-iemts-center p-4 text-decoration-none"
+            className= "d-flex text-neutral-grey justify-content-between align-iemts-center p-4 text-decoration-none"
           >
           <div className="d-flex align-items-center">
           <CgIcons.CgList />
@@ -164,18 +126,24 @@ return (
           {
             subNav[1] &&
               <div>
-                <DropdownLink to="/home/customers">
+                <Link 
+                  to="/home/customers"
+                  className = "px-4 mx-4 py-2 d-flex align-items-center text-decoration-none text-neutral-grey"
+                >
                   <span> Browse All </span>
-                </DropdownLink>
-                <DropdownLink to="/home/customers/create">
+                </Link>
+                <Link 
+                  to="/home/customers/create"
+                  className = "px-4 mx-4 py-2 d-flex align-items-center text-decoration-none text-neutral-grey"
+                >
                   <span> Add New </span>
-                </DropdownLink>
+                </Link>
               </div>
           }
         </div>
       }
-		</SidebarWrap>
-		</SidebarNav>
+		</div>
+		</nav>
 	</IconContext.Provider>
 	</>
 );
