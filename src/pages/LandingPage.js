bb6f73b75b3676340fe85cdbd32ad7430d91e6ff
denisesenguel <denisesenguel@gmail.com>
2022-03-08 16:55:26 +0100

    fix landing page height

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,7 +6,7 @@ export default function LandingPage() {
     return (
         <div>
             <NavBar />
-            <div className="fix-content-height bg-forest text-neutral-grey d-flex flex-column justify-content-center align-items-center">
+            <div className="vh-100 bg-forest text-neutral-grey d-flex flex-column justify-content-center align-items-center">
                 <h1>Reforestation at Scale</h1>
                 <h4 className="font-roboto-slab">Using Drones and Pelletized Seeds</h4>
             </div>
