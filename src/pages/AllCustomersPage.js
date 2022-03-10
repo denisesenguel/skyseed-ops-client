8c198f33ecb40d475468151e95182be202f161e8
denisesenguel <denisesenguel@gmail.com>
2022-03-10 09:52:14 +0100

    fix: customer list, now using a helper function for filtering

--- src/pages/AllCustomersPage.js
+++ src/pages/AllCustomersPage.js
@@ -5,7 +5,7 @@ import exampleImg from '../images/forest_bg_website.jpg';
 import ButtonMailTo from '../components/ButtonMailTo';
 import SuccessToast from '../components/SuccessToast';
 import useShowSuccess from '../hooks/useShowSuccess';
-import { generateAlphabet } from '../utils/helpers';
+import { generateAlphabet, lastNameStartsWith } from '../utils/helpers';
 import { Spinner } from 'react-bootstrap';
 
 export default function CustomersPage() {
@@ -46,10 +46,10 @@ export default function CustomersPage() {
                   <ListGroup key={ index } className="mb-1 shadow">
                     <ListGroup.Item className="bg-neutral-grey text-secondary-cstm"> { letter } </ListGroup.Item>
                     {
-                      (customers.filter(customer => customer.lastName.toUpperCase().indexOf(letter) > -1).length === 0) ?
+                      (customers.filter(customer => lastNameStartsWith(customer.lastName, letter)).length === 0) ?
                         <ListGroup.Item className="text-muted"> None </ListGroup.Item> :
                         customers
-                          .filter(customer => customer.lastName.toUpperCase().indexOf(letter) > -1)
+                          .filter(customer => lastNameStartsWith(customer.lastName, letter))
                           .map((customer, index) => (
                             <ListGroup.Item className="text-primary-cstm d-flex align-items-center p-3" key={ index }>
                               <img className="rounded-circle fix-img-height" src={ exampleImg } alt={ customer.lastName } />
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,4 +2,13 @@ function generateAlphabet(capital = true) {
 	return [...Array(26)].map((_, i) => String.fromCharCode(i + (capital ? 65 : 97)));
 }
 
-export { generateAlphabet };
+// check if a last name starts with an letter (provided uppercased).
+// ignore titles and case in last name
+function lastNameStartsWith(name, letter) {
+	let nameCapital = name.toUpperCase();
+	const ignore = ['VON ', 'VAN ', 'ZU ', 'DEM ', 'DEN ', 'ZUM '];
+	ignore.forEach(item => nameCapital = nameCapital.replace(item, ''));
+	return nameCapital.trim()[0]  === letter;
+}
+
+export { generateAlphabet, lastNameStartsWith };
